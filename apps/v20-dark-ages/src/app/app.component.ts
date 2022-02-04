094f2f4ddf02ddd6222c7caed25e1073399047fc
Sebastian Tremmel <littleperlmonk@gmail.com>
2022-02-05 00:03:13 +0100

    adding demo navbar to app

--- apps/v20-dark-ages/src/app/app.component.ts
+++ apps/v20-dark-ages/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuButton } from '@timesink/ui';
 
 @Component({
   selector: 'timesink-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'v20-dark-ages';
+
+  menuButton: MenuButton = {
+    name: 'test',
+    entries: ['entry A', 'entry B', 'entry C', 'entry D'],
+  };
+
+  menuButtonB: MenuButton = {
+    name: 'hello world 2',
+    entries: ['entry E', 'entry F', 'entry G', 'entry H', 'entry I'],
+  };
+
+  menuButtons: MenuButton[] = [this.menuButton, this.menuButtonB];
 }
--- apps/v20-dark-ages/src/app/app.module.ts
+++ apps/v20-dark-ages/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UiModule } from '@timesink/ui';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, UiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
