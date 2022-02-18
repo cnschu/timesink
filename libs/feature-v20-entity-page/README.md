1ad9e6285247ac44a1c974ec380a37460dedd2fc
Sebastian Tremmel <littleperlmonk@gmail.com>
2022-02-18 17:34:53 +0100

    feat: moved display of a character into its own component

---
+++ libs/feature-v20-entity-page/README.md
@@ -0,0 +1,7 @@
+# feature-v20-entity-page
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test feature-v20-entity-page` to execute the unit tests.
---
+++ libs/feature-v20-entity-page/src/lib/v20-entity-page/v20-entity-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { V20EntityPageComponent } from './v20-entity-page.component';
+
+describe('V20EntityPageComponent', () => {
+  let component: V20EntityPageComponent;
+  let fixture: ComponentFixture<V20EntityPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ V20EntityPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(V20EntityPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
